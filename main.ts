b30662203ad6add9f82e213bc04646123e8b1b76
David333100 <matiasovsky.david@gmail.com>
2025-06-30 00:23:54 +0200

    Aktualizovať main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,17 @@
 input.onGesture(Gesture.ScreenUp, function () {
-    kontrola_pre_pozastavenie_hry("abc", false)
+    funkcie_mas_dobru_polohu("abc", true)
 })
 input.onGesture(Gesture.ScreenDown, function () {
-    kontrola_pre_pozastavenie_hry("abc", true)
+    funkcie_mas_dobru_polohu("abc", false)
 })
 input.onGesture(Gesture.TiltRight, function () {
 	
 })
-function kontrola_pre_pozastavenie_hry (text: string, bool: boolean) {
-    if (false) {
+radio.onReceivedValue(function (name, value) {
+	
+})
+function funkcie_mas_dobru_polohu (text: string, bool: boolean) {
+    if (bool == false) {
         game.pause()
     } else {
         game.resume()
@@ -17,20 +20,37 @@ function kontrola_pre_pozastavenie_hry (text: string, bool: boolean) {
 input.onGesture(Gesture.TiltLeft, function () {
 	
 })
-basic.showString("Hru je nutne si nastavit")
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
-basic.showString("Tuto akciu mozno urobit pomocou...")
-basic.forever(function () {
-	
+for (let index = 0; index < 4; index++) {
+    basic.showIcon(IconNames.House)
+    basic.clearScreen()
+}
+loops.everyInterval(10, function () {
+    if (game.isRunning()) {
+        if (input.isGesture(Gesture.Shake) || input.isGesture(Gesture.TiltLeft) || input.isGesture(Gesture.TiltRight)) {
+        	
+        } else {
+        	
+        }
+    }
 })
-loops.everyInterval(100, function () {
-    if (game.isPaused()) {
-    	
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A) && input.buttonIsPressed(Button.B)) {
+        basic.clearScreen()
+        basic.pause(5000)
+    }
+    while (!(input.isGesture(Gesture.Shake))) {
+        basic.showString("Hru je nutne si nastavit")
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showString("Tuto akciu mozno urobit pomocou...")
+    }
+    while (input.isGesture(Gesture.Shake)) {
+        basic.clearScreen()
+        basic.pause(5000)
     }
 })
